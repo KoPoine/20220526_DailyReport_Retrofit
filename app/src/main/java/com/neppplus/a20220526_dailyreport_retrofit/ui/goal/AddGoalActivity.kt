56b21c64ad90b1271dcf4f3620ffbb144ab39d95
김민철 <kmc901206@gmail.com>
2022-05-27 15:24:52 +0900

    골 추가 xml 작업

---
+++ app/src/main/java/com/neppplus/a20220526_dailyreport_retrofit/ui/goal/AddGoalActivity.kt
@@ -0,0 +1,12 @@
+package com.neppplus.a20220526_dailyreport_retrofit.ui.goal
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.neppplus.a20220526_dailyreport_retrofit.R
+
+class AddGoalActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_goal)
+    }
+}
