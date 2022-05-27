181c8592f12ead39f8cfa60a889c58fbc3c2ce46
김민철 <kmc901206@gmail.com>
2022-05-27 12:12:49 +0900

    Patch로 회원 정보 수정 API 작성

--- app/src/main/java/com/neppplus/a20220526_dailyreport_retrofit/ui/setting/DetailProfileActivity.kt
+++ app/src/main/java/com/neppplus/a20220526_dailyreport_retrofit/ui/setting/DetailProfileActivity.kt
@@ -2,11 +2,13 @@ package com.neppplus.a20220526_dailyreport_retrofit.ui.setting
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import com.bumptech.glide.Glide
 import com.neppplus.a20220526_dailyreport_retrofit.BaseActivity
 import com.neppplus.a20220526_dailyreport_retrofit.R
 import com.neppplus.a20220526_dailyreport_retrofit.databinding.ActivityDetailProfileBinding
+import com.neppplus.a20220526_dailyreport_retrofit.dialog.CustomAlertDialog
 import com.neppplus.a20220526_dailyreport_retrofit.ui.main.LoginActivity
 import com.neppplus.a20220526_dailyreport_retrofit.utils.ContextUtil
 import com.neppplus.a20220526_dailyreport_retrofit.utils.GlobalData
@@ -32,6 +34,26 @@ class DetailProfileActivity : BaseActivity() {
             val myIntent = Intent(mContext, LoginActivity::class.java)
             myIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK).addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
             startActivity(myIntent)
+        }
+
+        binding.changeNickBtn.setOnClickListener {
+            val alert = CustomAlertDialog(mContext, this)
+            alert.myDialog(
+                "닉네입 변경",
+                "변경할 닉네임을 입력해 주세요.",
+                false,
+                object : CustomAlertDialog.ButtonClickListener{
+                    override fun positiveBtnClicked() {
+                        val changedNick = alert.binding.contentEdt.text.toString()
+
+                        Toast.makeText(mContext, changedNick, Toast.LENGTH_SHORT).show()
+                    }
+
+                    override fun negetiveBtnClicked() {
+
+                    }
+                }
+            )
         }
     }
 
