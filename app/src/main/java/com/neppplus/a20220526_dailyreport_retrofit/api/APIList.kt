1f5c667df725e3c77adf17d8c99cdec39ea46c3a
김민철 <kmc901206@gmail.com>
2022-05-27 16:14:25 +0900

    서버 API 제작 > 목표 추가 > 홈프래그먼트에 어떻게 인식을 시킬건가?

--- app/src/main/java/com/neppplus/a20220526_dailyreport_retrofit/api/APIList.kt
+++ app/src/main/java/com/neppplus/a20220526_dailyreport_retrofit/api/APIList.kt
@@ -7,34 +7,48 @@ import retrofit2.http.*
 
 interface APIList {
 
-//    user
+    //    user
     @FormUrlEncoded
     @PATCH("/user")
-    fun patchRequestEditUser (
-        @Header("X-Http-Token") token : String,
-        @Field("nick_name") nickname : String?,
-        @Field("current_password") currentPw : String?,
-        @Field("new_password") newPw : String?,
-    ) : Call<BasicResponse>
+    fun patchRequestEditUser(
+        @Header("X-Http-Token") token: String,
+        @Field("nick_name") nickname: String?,
+        @Field("current_password") currentPw: String?,
+        @Field("new_password") newPw: String?,
+    ): Call<BasicResponse>
 
     @FormUrlEncoded  // 파라미터 중에 폼데이터로 담아야하는 파라미터가 있다면 필요한 구문
     @POST("/user")  // retrofit이 POST라는 메쏘드로 적는구나
-    fun postRequestLogin (
+    fun postRequestLogin(
         @Field("email") email: String,
         @Field("password") pw: String
-    ) : Call<BasicResponse>
+    ): Call<BasicResponse>
 
     @FormUrlEncoded
     @PUT("/user")
-    fun putRequestSignUp (
-        @Field("email")email: String,
-        @Field("password")pw :String,
-        @Field("nick_name") nick : String
-    ) : Call<BasicResponse>
+    fun putRequestSignUp(
+        @Field("email") email: String,
+        @Field("password") pw: String,
+        @Field("nick_name") nick: String
+    ): Call<BasicResponse>
 
-//    main
+    //    goal
+    @FormUrlEncoded
+    @POST("/goal")
+    fun postRequestAddGoal(
+        @Header("X-Http-Token") token : String,
+        @Field("title") title : String,
+        @Field("color_value") color_value : String,
+        @Field("group_id") group_id : String,
+        @Field("goal_hour") goal_hour : String,
+        @Field("goal_minute") goal_minute : String,
+    ): Call<BasicResponse>
+
+    //    main
     @GET("/main_info")
     fun getRequestMainInfo(
-        @Header("X-Http-Token")token : String
-    ) : Call<BasicResponse>
+        @Header("X-Http-Token") token: String
+    ): Call<BasicResponse>
+
+
 }
--- app/src/main/java/com/neppplus/a20220526_dailyreport_retrofit/ui/goal/AddGoalActivity.kt
+++ app/src/main/java/com/neppplus/a20220526_dailyreport_retrofit/ui/goal/AddGoalActivity.kt
@@ -2,11 +2,19 @@ package com.neppplus.a20220526_dailyreport_retrofit.ui.goal
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.databinding.DataBindingUtil
 import com.neppplus.a20220526_dailyreport_retrofit.BaseActivity
 import com.neppplus.a20220526_dailyreport_retrofit.R
 import com.neppplus.a20220526_dailyreport_retrofit.databinding.ActivityAddGoalBinding
+import com.neppplus.a20220526_dailyreport_retrofit.models.BasicResponse
+import com.neppplus.a20220526_dailyreport_retrofit.utils.ContextUtil
+import com.neppplus.a20220526_dailyreport_retrofit.utils.GlobalData
+import org.json.JSONObject
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class AddGoalActivity : BaseActivity() {
 
@@ -20,7 +28,38 @@ class AddGoalActivity : BaseActivity() {
     }
 
     override fun setupEvents() {
+        customBinding.saveBtn.setOnClickListener {
+            val goalTitle = binding.titleEdt.text.toString()
+            apiList.postRequestAddGoal(
+                ContextUtil.getLoginUserToken(mContext),
+                goalTitle,
+                "#4FC3F7",
+                GlobalData.loginUser!!.groups[0].id.toString(),
+                "1",
+                "00"
+            ).enqueue(object : Callback<BasicResponse> {
+                override fun onResponse(
+                    call: Call<BasicResponse>,
+                    response: Response<BasicResponse>
+                ) {
+                    if (response.isSuccessful) {
 
+                    }
+                    else {
+                        val jsonObj = JSONObject(response.errorBody()!!.string())
+                        val code = jsonObj.getInt("code")
+                        val message = jsonObj.getString("message")
+
+                        Log.e("${TAG}서버에러 code", code.toString())
+                        Log.e("${TAG}서버에러 message", message)
+                    }
+                }
+
+                override fun onFailure(call: Call<BasicResponse>, t: Throwable) {
+
+                }
+            })
+        }
     }
 
     override fun setValues() {
