be94fd49cc25f7d9ce143a25930f72bd1b0a8a00
김민철 <kmc901206@gmail.com>
2022-05-27 10:36:36 +0900

    로그인한 유저 정보를 가져다 쓰는 GlobalData를 생성

---
+++ app/src/main/java/com/neppplus/a20220526_dailyreport_retrofit/utils/GlobalData.kt
@@ -0,0 +1,13 @@
+package com.neppplus.a20220526_dailyreport_retrofit.utils
+
+import com.neppplus.a20220526_dailyreport_retrofit.models.UserData
+
+class GlobalData {
+
+    companion object {
+
+        var loginUser : UserData? = null
+
+    }
+
+}
