e925e3a922b72267458c96f4267d6243edc55ca0
김민철 <kmc901206@gmail.com>
2022-05-27 10:06:31 +0900

    SplashActivity 자동로그인 로직 작성

--- app/src/main/java/com/neppplus/a20220526_dailyreport_retrofit/SplashActivity.kt
+++ app/src/main/java/com/neppplus/a20220526_dailyreport_retrofit/SplashActivity.kt
@@ -1,19 +1,71 @@
 package com.neppplus.a20220526_dailyreport_retrofit
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.util.Log
+import com.neppplus.a20220526_dailyreport_retrofit.models.BasicResponse
+import com.neppplus.a20220526_dailyreport_retrofit.utils.ContextUtil
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class SplashActivity : BaseActivity() {
+
+    var isTokenOk = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        setupEvents()
+        setValues()
     }
 
     override fun setupEvents() {
 
+//        토큰의 유효성 검사
+//        1) 서버에 토큰으로 로그인 할 수 있는 API가 있는가?
+//        2) 파라미터로 토큰만 받아서 접속 할 수 있는가?
+//        3) 로그인시 내려주는 response와 비슷한 response를 내려주는가?
+        apiList.getRequestMainInfo(
+            ContextUtil.getLoginUserToken(mContext))
+            .enqueue(object : Callback<BasicResponse>{
+                override fun onResponse(
+                    call: Call<BasicResponse>,
+                    response: Response<BasicResponse>
+                ) {
+                    if (response.isSuccessful) {
+                        isTokenOk = true
+                    }
+                }
+
+                override fun onFailure(call: Call<BasicResponse>, t: Throwable) {
+
+                }
+            })
     }
 
     override fun setValues() {
+        val myHandler = Handler(Looper.getMainLooper())
 
+        myHandler.postDelayed({
+            val myIntent : Intent
+
+            val isAutoLoginOk = ContextUtil.getAutoLogin(mContext)
+
+            if (isAutoLoginOk && isTokenOk) {
+                myIntent = Intent(mContext, MainActivity::class.java)
+            }
+            else {
+                myIntent = Intent(mContext, LoginActivity::class.java)
+            }
+
+            startActivity(myIntent)
+            finish()
+
+
+        }, 1500)
     }
 }
